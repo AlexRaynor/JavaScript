03a21ad3b9329c4abee2c3e6b0da57a6d1579043
AlexRaynor <36274821+AlexRaynor@users.noreply.github.com>
2018-09-10 22:00:08 +0300

    Add files via upload

---
+++ lesson_7_add/js/script.js
@@ -0,0 +1,11 @@
+setInterval(function(){
+	var date = new Date();
+
+	var format = [
+		(date.getHours()),
+		(date.getMinutes()),
+		(date.getSeconds())
+	].join(":");
+
+	document.getElementById("clock").innerHTML = format;
+}, 900);
